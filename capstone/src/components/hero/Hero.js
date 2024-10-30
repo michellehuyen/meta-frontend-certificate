60a6cc0190dc71666e3eb759e447f2de5be4ce3f
michellehuyen <nguyemi4@oregonstate.edu>
2024-10-30 14:53:23 -0700

    editing hero section

--- capstone/src/components/hero/Hero.js
+++ capstone/src/components/hero/Hero.js
@@ -5,8 +5,10 @@ const Hero = () => {
     return (
         <section className={styles.hero}>
             <div>
-                <h1>Little Lemon</h1>
-                <h2>Chicago</h2>
+                <span className={styles.title}>
+                    <h1>Little Lemon</h1>
+                    <h2>Chicago</h2>
+                </span>
 
                 <p>Based in Chicago, Illinois, Little Lemon is a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.
                         The chefs draw inspiration from <em>Italian</em>, <em>Greek</em>, and <em>Turkish</em> culture and have a menu of 12–15 items that they rotate seasonally.
@@ -15,7 +17,6 @@ const Hero = () => {
                 <p>Little Lemon is owned by two Italian brothers, Mario and Adrian, who moved to the United States to pursue their shared dream of owning a restaurant.
                         To craft the menu, Mario relies on family recipes and his experience as a chef in Italy. Adrian does all the marketing for the restaurant and led the effort to expand the menu beyond classic Italian to incorporate additional cuisines from the Mediterranean region.
                 </p>
-
             </div>
 
             
---
+++ capstone/src/components/menu/Menu.js
@@ -0,0 +1,11 @@
+import styles from "./styles.module.css";
+
+const Menu = () => {
+    return (
+        <section>
+            
+        </section>
+    );
+}
+
+export default Menu;
